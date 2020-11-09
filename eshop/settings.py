acfe6f9422580fc80cd1301aa90d79f450c6dbbe
Christer Granbäck <crille_granback@hotmail.com>
2020-11-09 19:57:54 +0000

    More deployment to Heroku

--- eshop/settings.py
+++ eshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=2)@3$x-v!-eactxk-79#*adr*^0%q_$c-*$$lcw9tq#(!kw!@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chris-of-swe.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -118,12 +118,18 @@ WSGI_APPLICATION = 'eshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
